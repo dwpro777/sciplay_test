ceb9bc26bea5835adec52e804f9cb2c550fbe736
Dwayne <dwayne.geller@apricityhealth.com>
2020-05-18 10:07:25 -0500

    separate out the logic from the UI side

--- CardGame/Data/BlackjackService.cs
+++ CardGame/Data/BlackjackService.cs
@@ -6,29 +6,44 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 
 namespace CardGame.Data
 {
+
+    public class BindingObject
+    {
+        public Player Player { get; set; }
+        public List<Hand> PlayerHands { get; set; }
+        public Hand DealerHand { get; set; }
+        public bool HasWinner = false;
+        public GameLib.HandOutcome HandOutcome;
+    } 
+
     public class BlackjackService
     {
 
         private readonly Blackjack Blackjack = new Blackjack();
-        public readonly Player Dealer = new Player() { Name = "dealer" };
+        public readonly Player Dealer = new Player() { Name = "Dealer" };
         private Hand DealerHand { get; set; }
+        public BindingObject Bindings { get; private set; }
 
         public BlackjackService()
         {
             Blackjack.AddPlayer(Dealer);
+            Bindings = new BindingObject() { };
         }
 
-        public Player JoinGame()
+        public BindingObject JoinGame()
         {
             var newPlayer = new Player() { Name = "Player"};
             Blackjack.AddPlayer(newPlayer);
-            return newPlayer;
+            Bindings.Player = newPlayer;
+            return Bindings;
+
         }
 
         public List<Hand> GetHands()
@@ -36,39 +51,72 @@ namespace CardGame.Data
             return Blackjack.PlayerHands;
         }
 
-        public List<Hand> Deal()
+        public void Deal()
         {
+            Bindings.HasWinner = false;
             Blackjack.PlayerHands.ForEach(hand => Blackjack.RemovePlayerHand(hand));
             Blackjack.Players.ForEach(player => Blackjack.AddPlayerHand(player));
             Blackjack.Shuffle();
             DealerHand = Blackjack.PlayerHands.FirstOrDefault(hand => hand.Player == Dealer);
+            Bindings.DealerHand = DealerHand;
+            Bindings.PlayerHands= Blackjack.PlayerHands.Where(hand => hand != DealerHand).ToList();
+
             Blackjack.PlayerHands.ForEach(hand => Blackjack.DealToPlayer(2, hand));
+
+            CheckForWinner();
+            if(!Bindings.HasWinner)
+                DealerHand.Cards.Last().IsVisible = false;
             //hide the dealers 2nd card(in a real implementation this wouldn't go to the client)
-            DealerHand.Cards.Last().IsVisible = false;
 
-            return GetHands();
         }
 
-        public int Hit(Hand hand)
+        public void Hit(int handIndex = 0)
         {
-            Blackjack.DealToPlayer(1, hand);
-            return Blackjack.GetBestScore(hand);
+            if (Bindings.PlayerHands.Count >= handIndex)
+            {
+                var currentHand = Bindings.PlayerHands[handIndex];
+                Blackjack.DealToPlayer(1, currentHand);
+                CheckForWinner(handIndex);
+            }
+
         }
 
-        public HandOutcome GetHandOutcome(Hand hand)
+        private void CheckForWinner(int handIndex = 0, bool playerIsStaying = false)
         {
-            return Blackjack.GetHandOutcome(hand, DealerHand);
+            if (Bindings.PlayerHands.Count >= handIndex)
+            {
+                var currentHand = Bindings.PlayerHands[handIndex];
+
+                var score = Blackjack.GetBestScore(currentHand);
+                if (score == -1)
+                {
+                    Bindings.HandOutcome = HandOutcome.Lose;
+                    Bindings.HasWinner = true;
+                    return;
+                }
+                else if(currentHand.Cards.Count == 2 && score == 21)
+                {
+                    Bindings.HandOutcome = HandOutcome.Win;
+                    Bindings.HasWinner = true;
+                }
+                else if (playerIsStaying)
+                {
+                    Bindings.HandOutcome = Blackjack.GetHandOutcome(currentHand, DealerHand);
+                    Bindings.HasWinner= true;
+                }
+
+            }
         }
 
         public int GetHandScore(Hand hand)
         {
             return Blackjack.GetBestScore(hand);
-
         }
 
-        public void Stay()
+        public void Stay(int handIndex = 0)
         {
             PlayDealerHand();
+            CheckForWinner(handIndex, true);
         }
 
         private void PlayDealerHand()
